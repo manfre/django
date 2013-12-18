a03340f9fad5649fa7436fa09ce29998bbc123af
Michael Manfre <mmanfre@gmail.com>
2013-12-18 12:19:05 -0500

    Skip model_forms.tests.UniqueTest.test_unique_null

--- tests/model_forms/tests.py
+++ tests/model_forms/tests.py
@@ -14,7 +14,7 @@ from django.db.models.query import EmptyQuerySet
 from django.forms.models import model_to_dict
 from django.utils._os import upath
 from django.utils.unittest import skipUnless
-from django.test import TestCase
+from django.test import TestCase, skipUnlessDBFeature
 from django.utils import six
 
 from .models import (Article, ArticleStatus, BetterWriter, BigInt, Book,
@@ -636,6 +636,7 @@ class UniqueTest(TestCase):
         self.assertEqual(len(form.errors), 1)
         self.assertEqual(form.errors['__all__'], ['Price with this Price and Quantity already exists.'])
 
+    @skipUnlessDBFeature('ignores_nulls_in_unique_constraints')
     def test_unique_null(self):
         title = 'I May Be Wrong But I Doubt It'
         form = BookForm({'title': title, 'author': self.writer.pk})
